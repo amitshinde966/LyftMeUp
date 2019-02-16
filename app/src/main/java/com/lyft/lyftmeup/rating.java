7e2756b1d3737ff7dff77b55e0455f80a049111d
KiranMandhare1997 <kiranmandhare1997@gmail.com>
2019-02-16 13:10:38 +0530

    Basic UI

--- app/src/main/java/com/lyft/lyftmeup/rating.java
+++ app/src/main/java/com/lyft/lyftmeup/rating.java
@@ -1,13 +1,37 @@
 package com.lyft.lyftmeup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class rating extends AppCompatActivity {
 
+
+    Button submit;
+    RatingBar ratingbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
+        submit = findViewById(R.id.btnsubmit);
+        ratingbar = findViewById(R.id.ratingBar);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String rating=String.valueOf(ratingbar.getRating());
+                Toast.makeText(getApplicationContext(), "ThankYou!! for rating : "+rating, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
     }
+
+
+
+
 }
